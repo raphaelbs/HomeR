f4c12bafa0b8d25d729cc9f397643210084425d6
Raphael Brandão <raphael.b.souza@hotmail.com>
2016-05-24 08:55:11 -0300

    Adicionando endereços e switches

--- NodeJS/lib/address.js
+++ NodeJS/lib/address.js
@@ -7,5 +7,11 @@ module.exports = {
     CONSUMO_LIQUIDO : 0,
     RENOVAVEL_BRUTO : 1,
     CONSUMO_DERIVADO : 2,
-    RENOVAVEL_DERIVADO : 3
+    RENOVAVEL_DERIVADO : 3,
+    CONSUMO : {
+        QUARTO : 4,
+        SALA : 5,
+        COZINHA : 6,
+        AREA : 7
+    }
 };
